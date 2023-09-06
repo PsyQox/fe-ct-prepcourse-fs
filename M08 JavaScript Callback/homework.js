9f5300dae487e1b929e2acac7f6f4b940be7bc5f
PsyQox <pikithor@gmail.com>
2023-09-06 14:33:20 -0500

    a

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -45,7 +45,7 @@ function map(array, cb) {
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
-   // Tu código:
+   // Tu código: 
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
